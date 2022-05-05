c83e4bc7a8b6875eb051d944c77a7d88c336cafc
Dani <dan8i8i8nov@gmail.com>
2022-05-05 22:55:28 +0300

    commit Daniil

---
+++ test_for_git.py
@@ -0,0 +1,9 @@
+#test file
+
+def myfunction(user_name):
+    print("You name - ", user_name)
+
+if __name__ == "__main__":
+
+    print("______________Start_Project________________")
+    myfunction("Daniil")
