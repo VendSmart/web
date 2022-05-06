783ace346c314ba8f89f8b87241168802252ae86
alexious <aleksey.arhipov17@gmail.com>
2022-05-06 13:30:23 +0300

    123

--- test_for_git.py
+++ test_for_git.py
@@ -4,6 +4,8 @@ def myfunction(user_name):
     print("You name - ", user_name)
     #my function is cool
 
+name = input('Введите имя')
+
 if __name__ == "__main__":
 
     print("______________Start_Project________________")
